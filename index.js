eb2d5758627a70a057523d5cd2e80d34cdb91e7e
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-11-11 13:51:45 -0500

    Create operations and use them from module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
@@ -17,24 +18,25 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
         console.log('Dropped collection', result);
 
-        const collection = db.collection('campsites');
-
-        collection.insertOne({name: 'Breadcrumb Trail Campground', description: 'Test'}, (err, result) => {
-            assert.strictEqual(err, null);
-
+        dboper.insertDocument(db, {name: 'Breadcrumb Trail Campground', description: 'Test'}, 'campsites', result => {
             console.log('Insert Document', result.ops);
-            
-            //nesting callbacks not necessarily the best
-            collection.find().toArray((err, docs) => {
-                assert.strictEqual(err, null);
-                console.log('Found Documents:', docs);
 
-                client.close();
+            dboper.findDocuments(db, 'campsites', docs => {
+                console.log('Found documents:', docs);
+
+                dboper.updateDocument(db, { name: 'Breadcrumb Trail Campground' }, { description: 'Updated Test Description' }, 'campsites', result => {
+                    console.log('Updated Document Count:', result.result.nModified);
+
+                    dboper.findDocuments(db, 'campsites', docs => {
+                        console.log('Found Documents:', docs);
+
+                        dboper.removeDocument(db, { name: 'Breadcrumb Trail Campground'}, 'campsites', result => {
+                            console.log('Deleted Documents Count:', result.deletedCount);
+                            client.close();
+                        });
+                    });
+                });
             });
-
         });
-
     });
-
-
 });
